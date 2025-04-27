df1c967e695fa6135369cf6ee487b540bf751082
OKUMUSH <furkanokumush@gmail.com>
2025-04-28 00:58:14 +0200

    Fixes incorrect template directory path
    Updates the Jinja2 template directory path to use an absolute
    path for improved consistency and compatibility across environments.

--- kreout-panel/app/routes/status_routes.py
+++ kreout-panel/app/routes/status_routes.py
@@ -10,7 +10,7 @@ import psutil
 import os
 
 router = APIRouter()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="/templates/")
 
 
 # Refactored status route to support multiple servers
